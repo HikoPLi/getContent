f55bc276a3cf5e64a30200694c2319fa0bf06b2d
LiHikoP <114278663+HikoPLi@users.noreply.github.com>
2023-03-30 10:12:19 +0800

    2023-03-30

---
+++ total/URL.py
@@ -0,0 +1,12 @@
+def URL(i):
+
+    URL = "https://hk.jobsdb.com/hk/search-jobs/python/" + str(i)
+
+    return URL
+
+
+def BASEURL():
+
+    BASEURL = "https://hk.jobsdb.com/hk/search-jobs/python/1"
+
+    return BASEURL
